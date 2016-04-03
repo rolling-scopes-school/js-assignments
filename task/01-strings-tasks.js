b948e0f986a8ab30efcf0c550f12fffe4c100345
ryzhik1995 <ryzhik1995@mail.ru>
2016-04-03 11:07:06 +0300

    I fixed the problem with a template

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -56,7 +56,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    return ("Hello, "+firstName+" "+lastName+"!");
+    return (`Hello, ${firstName} ${lastName}!`);
 }
 
 /**
