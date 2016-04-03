7c61e589b1dc4adfa95f9891eb0a3ec0c67d4e27
ryzhik1995 <ryzhik1995@mail.ru>
2016-04-03 18:45:56 +0300

    fixed angle between the vectors

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -73,8 +73,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    var angle = Math.atan2(y2, x2) - Math.atan2(y1, x1);
-    return angle < 0 ? angle + 2 * Math.PI : angle;
+    return Math.sqrt( Math.pow( (x1-x2), 2 ) + Math.pow( (y1-y2), 2 ) );
 }
 
 /**
@@ -112,8 +111,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-       return Math.acos( ( x1*x2 + y1*y2  ) / ( Math.hypot(x1,y1) * Math.hypot(x2,y2) ) );
-        // throw new Error('Not implemented');
+var angle = Math.atan2(y2, x2) - Math.atan2(y1, x1);
+return angle < 0 ? angle + 2 * Math.PI : angle;
 }
 
 /**
