e07157c9971188c3b0f257929d9e51570ba2490e
ryzhik1995 <ryzhik1995@mail.ru>
2016-04-04 12:16:51 +0300

    complied Tic-tac-toe,usual finger

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -516,7 +516,25 @@ function getMatrixProduct(m1, m2) {
  *
  */
 function evaluateTicTacToePosition(position) {
-    throw new Error('Not implemented');
+ if (  (position[0][0] == position[1][1] && position[0][0] == position[2][2])
+    || (position[0][1] == position[1][1] && position[0][1] == position[2][1])
+    || (position[1][0] == position[1][1] && position[1][0] == position[1][2])
+    || (position[2][0] == position[1][1] && position[2][0] == position[0][2]) ){
+    if (position[1][1] != undefined)
+    return position[1][1];
+}
+
+else if ( (position[0][2] == position[1][2] && position[0][2] == position[2][2])
+    || (position[2][0] == position[2][1] && position[2][0] == position[2][2]) ){
+    if (position[2][2] != undefined)
+    return position[2][2];
+}
+
+else if ( (position[0][0] == position[0][1] && position[0][0] == position[0][2]) 
+    || (position[0][0] == position[1][0] && position[0][0] == position[2][0]) )
+    return position[0][0];
+
+else  return undefined;
 }
 
 
