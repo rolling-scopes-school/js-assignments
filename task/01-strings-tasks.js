16968294aa4438dc100b1d382439d15a7924769a
pShlapakova <48383700+pShlapakova@users.noreply.github.com>
2019-03-10 01:31:47 +0300

    Update 01-strings-tasks.js

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -55,7 +55,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    return "Hello, ${firstName} ${lastName}!";      
+    return 'Hello, ${firstName} ${lastName}!';      
 }
 
 /**
