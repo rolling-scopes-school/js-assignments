6a0a9fba307bbb68c2beb9684b17ca89b8d822c4
pShlapakova <48383700+pShlapakova@users.noreply.github.com>
2019-03-11 03:04:32 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -116,7 +116,7 @@ function removeFalsyValues(arr) {
  *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
  */
 function getUpperCaseStrings(arr) {
-   return arr.forEach(function(elem) {elem.toUpperCase();});
+   return arr.map(String.toUpperCase);
 }
 
 
