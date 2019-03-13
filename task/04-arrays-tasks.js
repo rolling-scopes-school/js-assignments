6b1e414b96bee3960e23ecdf2d21e4d5b77a0320
pShlapakova <48383700+pShlapakova@users.noreply.github.com>
2019-03-14 02:12:18 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -235,7 +235,7 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {
-   return Array.from(arr, (elem, index) => arr.reduce(function(sum, elem){ sum += elem; return sum}, 0);
+   return Array.from(arr, (elem, index) => arr.reduce(function(sum, elem){ sum += elem; return sum;}, 0);
 }
 
 /**
