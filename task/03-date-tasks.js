adc99906b11656bc6bc9dc6f2d0836b91f247dd1
pShlapakova <48383700+pShlapakova@users.noreply.github.com>
2019-03-13 21:27:10 +0300

    Update 03-date-tasks.js

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -76,7 +76,8 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   return ((endDate - startDate).toISOString()).substring(11,23);
+   throw new Error('Not implemented');
+   /*return ((endDate - startDate).toISOString()).substring(11,23);*/
 }
 
 
