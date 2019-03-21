16e41b16961a13d3e428c772bdbbc1cccabdd451
pShlapakova <48383700+pShlapakova@users.noreply.github.com>
2019-03-21 15:52:06 +0300

    Update 05-regex-tasks.js

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -91,7 +91,7 @@ function getRegexForIPv4() {
  * @return {RegExp}
  */
 function getRegexForSSN() {
-   return /\d{2}[1-9]|[1-9]\d{2}|\d[1-9]\d-\d[1-9]|[1-9]\d-\d{3}[1-9]|[1-9]\d{3}|\d{2}[1-9]\d|\d[1-9]\d{2}/
+   return /(\d{2}[1-9]|[1-9]\d{2}|\d[1-9]\d)-(\d[1-9]|[1-9]\d)-(\d{3}[1-9]|[1-9]\d{3}|\d{2}[1-9]\d|\d[1-9]\d{2})/
 }
 
 
