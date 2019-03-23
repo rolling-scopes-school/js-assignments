ee38379f6e29325ba7c05225637ad8fe087352ad
pShlapakova <48383700+pShlapakova@users.noreply.github.com>
2019-03-23 22:17:50 +0300

    Update 02-numbers-tasks.js

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -225,7 +225,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-    return isNaN(value) ? def : value;
+    return (isNaN(value) || !value) ? def : value;
 }
 
 module.exports = {
