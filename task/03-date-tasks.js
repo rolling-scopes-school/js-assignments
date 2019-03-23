42b3ee9be326c55ff95f529e419f25f3657275ad
pShlapakova <48383700+pShlapakova@users.noreply.github.com>
2019-03-24 00:17:27 +0300

    Update 03-date-tasks.js

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -95,7 +95,7 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    let angle = Math.abs(Math.PI/6*(date.getUTCHours()%12) + (Math.PI/360- Math.PI/30)*date.getUTCMinutes());
+    let angle = Math.abs(Math.PI/6*(date.getUTCHours()%12) - 11*Math.PI/360*date.getUTCMinutes());
     return angle > Math.PI ? angle - Math.PI : angle;
 }
 
