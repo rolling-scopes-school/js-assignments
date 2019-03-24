81ec488a8f0768cbf54c4665a40791e16997fe8f
pShlapakova <48383700+pShlapakova@users.noreply.github.com>
2019-03-24 04:14:35 +0300

    Update 06-conditions-n-loops-tasks.js

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -131,7 +131,7 @@ function doRectanglesOverlap(rect1, rect2) {
     if (rect1.top > rect2.top)
         rect2 = (rect1 => rect1)(rect1, rect1 = rect2);
     return (rect2.top >= rect1.top) && (rect2.top <= rect1.top + rect1.height) 
-        && (rect2.left + rect2.width >= rect1.left) && (rect2.left + rect2.width <= rect1.left + rect1.width);
+        && (rect2.left + rect2.width >= rect1.left) && (rect2.left <= rect1.left + rect1.width);
 }
 
 
@@ -163,7 +163,7 @@ function doRectanglesOverlap(rect1, rect2) {
  */
 function isInsideCircle(circle, point) {
     return (point.x - circle.center.x)*(point.x - circle.center.x) +
-        (point.y - circle.center.y)*(point.y - circle.center.y) <= circle.radius*circle.radius;
+        (point.y - circle.center.y)*(point.y - circle.center.y) < circle.radius*circle.radius;
 }
 
 
