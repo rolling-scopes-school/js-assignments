c624cd1b77eb27c361fe6086376cce6ec28c5c45
pShlapakova <48383700+pShlapakova@users.noreply.github.com>
2019-04-08 08:32:24 +0300

    Update 02-numbers-tasks.js

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -203,7 +203,8 @@ function roundToPowerOfTen(num, pow) {
  */
 function isPrime(n) {
     if (n > 1)
-        for (var i=2; i<=Math.sqrt(n); i++)
+        if (n % 2 == 0) return false;
+        for (var i=3; i<=Math.sqrt(n); i += 2)
             if (n % i == 0)
                 return false;
     return true;
