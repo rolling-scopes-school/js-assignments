ab05b76a010b8cb3a1dc6f111730b874355530b1
rinat_gt <pacmanreal@gmail.com>
2019-07-31 17:58:56 +0500

    refactor: 03-date-tasks almost done

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+    return new Date(value);
 }
 
 
@@ -56,7 +56,7 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+    return ((date.getFullYear() % 4 == 0) && (date.getFullYear() % 100 != 0)) || (date.getFullYear() % 400 == 0);
 }
 
 
@@ -76,7 +76,19 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+    let res = endDate.getTime() - startDate.getTime();
+    function pad(n, z) {
+        z = z || 2;
+        return ('00' + n).slice(-z);
+    }
+    let ms = res % 1000;
+    res = (res - ms) / 1000;
+    let secs = res % 60;
+    res = (res - secs) / 60;
+    let mins = res % 60;
+    let hrs = (res - mins) / 60;
+
+    return pad(hrs) + ':' + pad(mins) + ':' + pad(secs) + '.' + pad(ms, 3);
 }
 
 
@@ -94,7 +106,15 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    let h = date.getHours();
+    let m = date.getMinutes();
+    if (h == 12) h = 0;
+    if (m == 60) m = 0;
+    let hour_angle = 0.5 * (h * 60 + m);
+    let minute_angle = 6 * m;
+    let angle = Math.abs(hour_angle - minute_angle);
+    angle = Math.min(360 - angle, angle);
+    return angle;
 }
 
 
