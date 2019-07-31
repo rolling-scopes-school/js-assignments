6250ed5bc91502f4bdcf943566ff64731ae26d15
Elbek Normurodov <normurodovelbek0133@gmail.com>
2019-07-31 17:14:17 +0500

    fix: error in date tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,8 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   let date = new Date.parse(value);
-   return date;
+   throw new Error('Not implemented');
 }
 
 /**
