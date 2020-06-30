47f5b28109166f965f8284e35ed03dbda6b77d1e
zym <zymlagzom@gmail.com>
2020-06-30 21:11:16 +0300

    Completed 3 task

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const moment = require("moment");
+
 /********************************************************************************************
  *                                                                                          *
  * Plese read the following tutorial before implementing tasks:                             *
@@ -22,7 +24,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 /**
@@ -37,7 +39,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 
@@ -56,7 +58,10 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let year= date.getFullYear();
+
+   return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+
 }
 
 
@@ -76,7 +81,17 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   
+   var difDate = new Date(endDate-startDate).toISOString();
+   var takeDay= new Date(endDate-startDate).getDate();
+   var subDate = difDate.substr(difDate.indexOf('T')+1,difDate.indexOf('Z')-difDate.indexOf('T')-1);
+   if(takeDay>1){
+      var massDate = subDate.split(':');
+      massDate[0]=parseInt(massDate[0])+24*(takeDay-1);
+      var resDate=massDate.join(':');
+      return resDate;
+   }
+   return subDate;
 }
 
 
@@ -93,8 +108,18 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
+
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   function lerp (a, b, c) {
+      return a + c * (b - a);
+      }
+   
+   var hours = date.getHours()-3;
+   var minutes = date.getMinutes();
+   var anglePosH=hours % 12 +(minutes/60);
+   var anglePosM=lerp(0,12,minutes/60);
+   var result=Math.min(Math.abs(anglePosH-anglePosM)*(2*Math.PI)/12,Math.abs(2*Math.PI-Math.abs(anglePosH-anglePosM)*(2*Math.PI)/12));
+   return result;
 }
 
 
