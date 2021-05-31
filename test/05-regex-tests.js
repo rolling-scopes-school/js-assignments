941d569e753b4af7317b24780ad9e5eb5f6f6a81
Taras Nevostruev <taris.tarasov@yandex.ru>
2021-05-31 06:57:39 +0300

    Task5

--- test/05-regex-tests.js
+++ test/05-regex-tests.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var assert = require('assert');
+var assert = require('assert') ;
 var tasks = require('../task/05-regex-tasks');
 it.optional = require('../extensions/it-optional');
 
