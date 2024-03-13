03849a98f04da613b94602dd859e74f2edb61e5b
Anna5194 <45049285+Anna5194@users.noreply.github.com>
2024-03-13 10:57:41 +0300

    Update 03-date-tasks.js
    03-date-tasks решения

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -5,7 +5,7 @@
  * Plese read the following tutorial before implementing tasks:                             *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Numbers_and_dates#Date_object
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date    *
- *                                                                                          *
+ *    npm test ./test/03-date-tests.js                                                                                      *
  ********************************************************************************************/
 
 
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 
@@ -56,7 +56,8 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let year = date.getFullYear();
+   return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
 
 
@@ -76,7 +77,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   return (new Date(endDate - startDate)).toISOString().slice(11, -1);
 }
 
 
@@ -94,7 +95,13 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   const hoursArrow = 0.5 * (60 * (date.getUTCHours() % 12) + date.getUTCMinutes());
+   const minutesArrow = 6 * date.getUTCMinutes();
+   let angle = Math.abs(hoursArrow - minutesArrow);
+   if (angle > 180) {
+       angle = 360 - angle;
+   }
+   return (angle * Math.PI / 180);
 }
 
 
